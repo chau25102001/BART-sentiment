59a783b6e7a743b5042dc7133bd90aec7f27c3ee
c2510 <chau.nm194420@sis.hust.edu.vn>
2023-01-19 20:16:15 +0700

    new-data

--- clean_data.py
+++ clean_data.py
@@ -1,18 +1,15 @@
+import os
+
 import pandas as pd
-from transformers import BartTokenizer
 import re
 import nltk
 
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 
-tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
-
-dataframe = pd.read_csv('/home/chaunm/PycharmProjects/datasets/IMDB Dataset.csv')
-
 
 def transform_label(label):
-    return 1 if label == 'positive' else 0
+    return 1 if label == 'pos' else 0
 
 
 def rm_link(text):
@@ -64,32 +61,27 @@ def spell_correction(text):
 def clean_pipeline(text):
     no_link = rm_link(text)
     no_html = rm_html(no_link)
-    no_punct = rm_punct2(no_html)
+    no_emoji = rm_emoji(no_html)
+    no_punct = rm_punct2(no_emoji)
     no_number = rm_number(no_punct)
     no_whitespaces = rm_whitespaces(no_number)
     no_nonasci = rm_nonascii(no_whitespaces)
-    no_emoji = rm_emoji(no_nonasci)
-    spell_corrected = spell_correction(no_emoji)
+    # no_emoji = rm_emoji(no_nonasci)
+    spell_corrected = spell_correction(no_nonasci)
     return spell_corrected
 
 
-dataframe['label'] = dataframe['sentiment'].apply(transform_label)
-dataframe['clean'] = dataframe['review'].apply(clean_pipeline)
-dataframe_pos = dataframe[dataframe['label'] == 1]
-dataframe_neg = dataframe[dataframe['label'] == 0]
-split_ratio = 0.8
-dataframe_pos_train = dataframe_pos.sample(frac=split_ratio, random_state=200)
-dataframe_pos_test = dataframe_pos.drop(dataframe_pos_train.index)
-dataframe_neg_train = dataframe_neg.sample(frac=split_ratio, random_state=200)
-dataframe_neg_test = dataframe_neg.drop(dataframe_neg_train.index)
+def clean_dataframe(path):
+    assert os.path.isfile(path), "data file does not exist, check it"
+    root = os.path.dirname(path)
+    name = path.split("/")[-1].split(".")[0]
+    dataframe = pd.read_csv(path)
+    dataframe['label'] = dataframe['sentiment'].apply(transform_label)
+    dataframe['clean'] = dataframe['text'].apply(clean_pipeline)
+    dataframe[['clean', 'label']].to_csv(os.path.join(root, name + "_clean.csv"), index=False, header=True)
 
-dataframe_train = pd.concat([dataframe_neg_train, dataframe_pos_train], ignore_index=True, axis=0)
-dataframe_test = pd.concat([dataframe_neg_test, dataframe_pos_test], ignore_index=True, axis=0)
-dataframe_train = dataframe_train.sample(frac=1)  # shuffle
-dataframe_test = dataframe_test.sample(frac=1)  # shuffle
-print(len(dataframe_train[dataframe_train['label'] == 1]))
-print(len(dataframe_test[dataframe_test['label'] == 1]))
-print(len(dataframe_train[dataframe_train['label'] == 0]))
-print(len(dataframe_test[dataframe_test['label'] == 0]))
-dataframe_train[['clean', 'label']].to_csv("./datasets/imdb_train.csv", index=False, header=True)
-dataframe_test[['clean', 'label']].to_csv("./datasets/imdb_test.csv", index=False, header=True)
+
+# clean_dataframe('./datasets/train.csv')
+# clean_dataframe('./datasets/test.csv')
+data_cleaned = pd.read_csv("./datasets/train_clean.csv")
+print(data_cleaned['clean'][0])
